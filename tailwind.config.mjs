5c36b24b5dec84961b5e1f2ac55cf5221709a25a
KatkaMarcincakova <katulik137@gmail.com>
2024-06-18 19:00:12 +0200

    setup

---
+++ tailwind.config.mjs
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+        screens: {
+            xxs: "340px",
+            xs: "410px",
+            sm: "480px",
+            s: "590px",
+            md: "720px",
+            m: "820px",
+            lg: "976px",
+            l: "1200px",
+            xl: "1440px",
+        },
+        extend: {
+            colors: {
+                primary: "#3E5541",
+                secondaty: "#DBD2CB",
+                tertiary: "#DAD4D4",
+                dark: "#0A0A0A",
+                light: "#FBFBFB",
+                tint: "#C4A36D",
+            },
+			fontFamily: {
+				'BrittanySignature': ['BrittanySignature', 'sans-serif'],
+			},
+        },
+    },
+	plugins: [],
+}
