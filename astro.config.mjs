0f65da806142956f3b780a4b47ba1f714fe86852
KatkaMarcincakova <katulik137@gmail.com>
2024-06-20 14:50:04 +0200

    changed url to new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://katkamarcincakova.github.io',
-  base: 'wedding',
+  site: 'https://svadba.devity.sk',
+  base: '',
 });
