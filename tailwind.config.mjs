3a80f1862f1abc73dd2dd82bb7d2284a55eeb49c
Samuel Neceda <69465264+SamuelNeceda@users.noreply.github.com>
2025-06-15 21:03:23 +0200

    alex font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -24,7 +24,7 @@ export default {
         hoverGold: "#1E3A8A", // Darker gold for hover
       },
       fontFamily: {
-        "Parisienne-Regular": ["Parisienne-Regular", "sans-serif"],
+        "AlexBrush-Regular": ["AlexBrush-Regular", "sans-serif"],
       },
     },
   },
